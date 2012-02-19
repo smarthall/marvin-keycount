104f2c0e3ff2af1a2df7dbfefaec3d4ab04f8d9e
Daniel Hall <daniel.hall@rea-group.com>
2012-02-19 16:32:10 +1100

    Check the return codes of things that could cause errors

--- x11keycount.c
+++ x11keycount.c
@@ -5,10 +5,22 @@
 
 x11keycount_t* x11keycount_init() {
     x11keycount_t *keycount;
+    int keymapret;
 
     keycount = malloc(sizeof(x11keycount_t));
+    if (keycount == NULL) {
+        return NULL;
+    }
+
     keycount->disp = XOpenDisplay(NULL);
-    XQueryKeymap(keycount->disp, keycount->last_keys);
+    if (keycount->disp == NULL) {
+        return NULL;
+    }
+
+    keymapret = XQueryKeymap(keycount->disp, keycount->last_keys);
+    if (keymapret == 0) {
+        return NULL;
+    }
 
     return keycount;
 }
@@ -22,9 +34,13 @@ int x11keycount_close(x11keycount_t *keycount) {
 
 int x11keycount_count(x11keycount_t *keycount, int *count) {
     char keys[32];
+    int keymapret;
     *count = 0;
 
-    XQueryKeymap(keycount->disp,keys);
+    keymapret = XQueryKeymap(keycount->disp,keys);
+    if (keymapret == 0) {
+        return EXIT_FAILURE;
+    }
     if (memcmp(keys, keycount->last_keys, sizeof(char) * 32) != 0) {
         for (int i = 0; i < 32; i++) {
             unsigned char diff = keys[i] ^ keycount->last_keys[i];
