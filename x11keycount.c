0844d004f54ef9ad4184a530449b2610cb4cc543
Daniel Hall <daniel@danielhall.me>
2012-03-05 09:10:29 +1100

    Track max pressed

--- x11keycount.c
+++ x11keycount.c
@@ -46,15 +46,19 @@ int x11keycount_poll(x11keycount_t *keycount) {
     if (memcmp(keys, keycount->last_keys, sizeof(char) * 32) != 0) {
         for (int i = 0; i < 32; i++) {
             unsigned char diff = keys[i] ^ keycount->last_keys[i];
+            unsigned char c, m;
             // Count different bits
-            unsigned char c;
             for (c = 0; diff; diff >>= 1)
             {
                 c += diff & 1;
             }
-            if (c > 0) {
-                keycount->buckets[keycount->cb] += c;
+            for (m = 0; keys[i]; keys[i] >>= 1)
+            {
+                m += keys[i] & 1;
             }
+            keycount->count[keycount->cb] += c;
+            if (m > keycount->max[keycount->cb])
+                keycount->max[keycount->cb] = m;
         }
         memcpy(keycount->last_keys, keys, sizeof(char) * 32);
         //TODO: Instead of memcopy, use two arrays and flip
@@ -62,7 +66,7 @@ int x11keycount_poll(x11keycount_t *keycount) {
 
     if (keycount->nb_time < time(NULL)) {
         keycount->cb = (keycount->cb + 1) % BUCKETS;
-        keycount->buckets[keycount->cb] = 0;
+        keycount->count[keycount->cb] = 0;
         keycount->nb_time = time(NULL) + BUCKTIME;
     }
 
@@ -73,9 +77,18 @@ unsigned long x11keycount_total(x11keycount_t *keycount) {
     unsigned long count = 0;
 
     for (int i = 0; i < BUCKETS; i++)
-        count += keycount->buckets[i];
+        count += keycount->count[i];
 
     return count;
+}
+
+int x11keycount_max(x11keycount_t *keycount) {
+    int max = 0;
+
+    for (int i = 0; i < BUCKETS; i++)
+        if (keycount->max[i] > max) max = keycount->max[i];
+
+    return max;
 }
 
 float x11keycount_average(x11keycount_t *keycount) {
--- x11keycount.h
+++ x11keycount.h
@@ -13,7 +13,8 @@
 typedef struct {
     Display *disp; // X11 Display
     char last_keys[32]; // The last key array from X11
-    unsigned int buckets[BUCKETS]; // The time buckets for the data
+    unsigned int count[BUCKETS]; // The time buckets for the data
+    unsigned int max[BUCKETS]; // The time buckets for the data
     int cb; // The current bucket
     time_t nb_time; // The epoch to move to the next bucket after
 } x11keycount_t;
@@ -21,6 +22,7 @@ typedef struct {
 x11keycount_t* x11keycount_init();
 int x11keycount_close(x11keycount_t *keycount);
 unsigned long x11keycount_total(x11keycount_t *keycount);
+int x11keycount_max(x11keycount_t *keycount);
 float x11keycount_average(x11keycount_t *keycount);
 int x11keycount_poll(x11keycount_t *keycount);
 
